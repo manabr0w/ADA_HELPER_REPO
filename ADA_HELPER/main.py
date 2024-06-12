520c21779015566d4832218368791014b6f11b2b
maximfedorenko <maximfedorenko71@gmail.com>
2024-06-12 20:49:28 +0300

    created colection instead if-else operators

--- ADA_HELPER/main.py
+++ ADA_HELPER/main.py
@@ -6,13 +6,32 @@ from matrix_math import matrix_operation
 from show_weather import get_weather_info
 
 def main():
-    list_of_operations = ["1. Вирішити лінійне рівняння",
-                          "2. Вирішити квадратне рівняння",
-                          "3. Знайти столицю країни",
-                          "4. Перевести число з десяткової в іншу систему",
-                          "5. Операції з матрицями",
-                          "6. Переглянути погоду в місті",
-                          "7. Завершити програму"]
+    list_of_operations = [
+        "1. Вирішити лінійне рівняння",
+        "2. Вирішити квадратне рівняння",
+        "3. Знайти столицю країни",
+        "4. Перевести число з десяткової в іншу систему",
+        "5. Операції з матрицями",
+        "6. Переглянути погоду в місті",
+        "7. Завершити програму"
+    ]
+
+    operations_dict = {
+        "1": solve_linear_equation,
+        "2": solve_quadratic,
+        "Вирішити квадратне рівняння": solve_quadratic,
+        "3": get_capital,
+        "Знайти столицю країни": get_capital,
+        "4": decimal_to_change,
+        "Перевести число з десяткової в іншу систему": decimal_to_change,
+        "5": matrix_operation,
+        "Операції з матрицями": matrix_operation,
+        "6": get_weather_info,
+        "Переглянути погоду в місті": get_weather_info,
+        "7": lambda: print("Програма завершена.") or exit(),
+        "завершити програму": lambda: print("Програма завершена.") or exit(),
+        "end": lambda: print("Програма завершена.") or exit()
+    }
 
     while True:
         print("Щоб вибрати операцію, введіть номер операції або назву українською мовою:")
@@ -20,21 +39,9 @@ def main():
             print(operation)
 
         question = input('Ваш вибір: ')
-        if question == "1":
-            solve_linear_equation()
-        elif question == "2" or question == "Вирішити квадратне рівняння":
-            solve_quadratic()
-        elif question == "3" or question == "Знайти столицю країни":
-            get_capital()
-        elif question == "4" or question == "Перевести число з десяткової в іншу систему":
-            decimal_to_change()
-        elif question == "5" or question == "Операції з матрицями":
-            matrix_operation()
-        elif question == "6" or question == "Переглянути погоду в місті":
-            get_weather_info()
-        elif question == "7" or question.lower() == "end" or question.lower() == "завершити програму":
-            print("Програма завершена.")
-            break
+        operation = operations_dict.get(question.lower())
+        if operation:
+            operation()
         else:
             print("Неправильний вибір операції.")
 
