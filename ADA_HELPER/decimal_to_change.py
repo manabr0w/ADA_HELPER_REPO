e612db6b5ce30ed36cd4565f6b5664e2b06a5866
maximfedorenko <maximfedorenko71@gmail.com>
2024-06-12 21:15:29 +0300

    used colection instead if - else operators

--- ADA_HELPER/decimal_to_change.py
+++ ADA_HELPER/decimal_to_change.py
@@ -1,5 +1,23 @@
+def decimal_to_binary(decimal):
+    return bin(decimal).replace("0b", "")
+
+
+def decimal_to_hexadecimal(decimal):
+    return hex(decimal).replace("0x", "")
+
+
+def decimal_to_octal(decimal):
+    return oct(decimal).replace("0o", "")
+
+
 def decimal_to_change():
     decimal = int(input("Введіть число у десятковій системі числення: "))
+
+    conversion_functions = {
+        "бінарна": decimal_to_binary,
+        "шістнадцяткова": decimal_to_hexadecimal,
+        "вісімкова": decimal_to_octal
+    }
 
     while True:
         print("Оберіть систему числення:")
@@ -8,19 +26,14 @@ def decimal_to_change():
         print("3. Вісімкова")
         choice = input("Ваш вибір (Бінарна/Шістнадцяткова/Вісімкова): ").lower()
 
-        if choice in ["бінарна", "1"]:
-            result = bin(decimal).replace("0b", "")
-            print(f"Двійкове представлення числа {decimal}: {result}")
-            break
-        elif choice == "шістнадцяткова":
-            result = hex(decimal).replace("0x", "")
-            print(f"Шістнадцяткове представлення числа {decimal}: {result}")
-            break
-        elif choice == "вісімкова":
-            result = oct(decimal).replace("0o", "")
-            print(f"Вісімкове представлення числа {decimal}: {result}")
+        conversion_function = conversion_functions.get(choice)
+        if conversion_function:
+            result = conversion_function(decimal)
+            print(f"Представлення числа {decimal} у вибраній системі: {result}")
             break
         else:
             print("Неправильний вибір. Спробуйте ще раз.")
 
 
+# Run the function
+decimal_to_change()
